dd9b37bd4e2a01eebc225f7f9dd93b2176c0f9e0
lovecrysong <a0923862606@gmail.com>
2018-12-19 16:26:34 +0800

    connect to heroku

--- app.py
+++ app.py
@@ -322,4 +322,4 @@ def show_fsm():
 
 
 if __name__ == "__main__":
-    run(host="0.0.0.0", port=PORT, debug=True, reloader=True)
+    app.run(host="0.0.0.0", port=PORT, debug=True, reloader=True)
--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import requests
 from urllib.request import urlopen
 
+
 GRAPH_URL = "https://graph.facebook.com/v2.6"
 ACCESS_TOKEN = "EAADZBPAz8j68BAOpAqQaAnytMkv6PhxQJ6FjHEMFY2OIqzEKah3Sdb0XVvmZCHZB4oeLOlxVsVRrBI67PslLf7ctrEOVhn35UE6RcBQxx4zvOp7qZCXW0orgZCFiZCXt3AOSi2rdlZAWqsXxlPIBDj4VWNIcWlzxUZC4L53ebgKcOgZDZD"
 
